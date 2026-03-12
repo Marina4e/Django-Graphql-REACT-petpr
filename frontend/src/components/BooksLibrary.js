4a3e171ded1325b0ba69b16262b244a26fd17bcc
Marina Che <acc13072021@gmail.com>
2026-03-12 14:18:17 +0200

    version-5 nice but with pagination

--- frontend/src/components/BooksLibrary.js
+++ frontend/src/components/BooksLibrary.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import {useQuery} from "@apollo/client/react";
 import {Link} from "react-router-dom";
 import {FETCH_BOOKS_QUERY} from "../queries";
 
 function BooksLibrary(){
 
+const [page,setPage] = useState(1)
+
 const {loading,error,data} = useQuery(FETCH_BOOKS_QUERY,{
 variables:{
-page:1,
-limit:20
+page:page,
+limit:8
 }
 })
 
@@ -16,6 +18,7 @@ if(loading) return <p>Loading...</p>
 if(error) return <p>{error.message}</p>
 
 const books = data.allBooks.books
+const totalPages = data.allBooks.pages
 
 return(
 
@@ -40,6 +43,28 @@ return(
 </Link>
 
 ))}
+
+</div>
+
+<div className="pagination">
+
+<button
+disabled={page===1}
+onClick={()=>setPage(page-1)}
+>
+Previous
+</button>
+
+<span>
+Page {page} of {totalPages}
+</span>
+
+<button
+disabled={page===totalPages}
+onClick={()=>setPage(page+1)}
+>
+Next
+</button>
 
 </div>
 
